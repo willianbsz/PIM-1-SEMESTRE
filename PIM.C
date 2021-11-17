087b2119ca802e46bb456a87fb86bbe97dc37f5c
willianbsz <welk1748@gmail.com>
2021-11-16 22:27:17 -0300

    Criação dos menus: Principal, cadastros e cadastro de usuários.

--- PIM.C
+++ PIM.C
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 #define TAM 100
+#define NOMETAM 100
 
 //==UTILS==
 void LimparTela()
@@ -12,15 +13,109 @@ void LimparTela()
     system("cls");
 }
 
+void LinhaDivisoria()
+{
+    printf("\n----------------------------------------------------------------------------\n");
+}
+
+void PularLinha()
+{
+    printf("\n");
+}
+
+void FecharPrograma()
+{
+    LimparTela();
+    printf("Ok, até a próxima!!!");
+    getch();
+    system("exit");
+}
+
+void MostrarTitulo(const char titulo[TAM])
+{
+    LinhaDivisoria();
+    printf(titulo);
+    LinhaDivisoria();
+    PularLinha();
+}
+
+// ATLETAS
+struct ATLETA
+{
+    char nome[NOMETAM];
+    int idade;
+    Modalidade modalidade;
+    Pais pais;
+    Equipe equipe;
+    Alojamento alojamento;
+    Medalha medalhas[TAM];
+} Atletas[TAM];
+
+// MEDALHA
+struct Medalha
+{
+    TipoMedalha tipo;
+};
+
+enum TipoMedalha
+{
+    Ouro,
+    Prata,
+    Bronze
+};
+
+// MODALIDADE
+struct Modalidade
+{
+    char nome[NOMETAM];
+} Modalidades[TAM];
+
+// PAIS
+struct Pais
+{
+    char nome[NOMETAM];
+} Paises[TAM];
+
+// EQUIPE
+struct Equipe
+{
+    char nome[NOMETAM];
+} Equipes[TAM];
+
+// ALOJAMENTO
+struct Alojamento
+{
+    char nome[NOMETAM];
+    Local local;
+} Alojamentos[TAM];
+
+// LOCAL
+struct Local
+{
+    char rua[NOMETAM];
+    int numero;
+} Localis[TAM];
+
+// FUNCIONARIO
+struct Funcionario
+{
+    char nome[NOMETAM];
+} Funcionarios[TAM];
+
+// VOLUNTARIO
+struct Voluntario
+{
+    char nome[NOMETAM];
+} Voluntarios[TAM];
+
 //==LOGIN==
 struct ModeloDeLogin
 {
     char usuario[50];
     char senha[8];
-};
+} usuarios[TAM];
 
 FILE *loginFile;
-ModeloDeLogin usuarios[TAM];
 
 FILE *AbrirTxt(FILE *arq, const char *nomeDoArquivo, const char *acao)
 {
@@ -45,7 +140,7 @@ ModeloDeLogin MostrarFormularioLogin(ModeloDeLogin login)
     return login;
 }
 
-void BuscarUsuario(ModeloDeLogin login)
+bool BuscarUsuario(ModeloDeLogin login)
 {
     loginFile = AbrirTxt(loginFile, "login.txt", "r");
 
@@ -54,9 +149,6 @@ void BuscarUsuario(ModeloDeLogin login)
 
     while (encontrado == 1)
     {
-        printf("Procurando na posição\n");
-        printf("%d", i + 1);
-        printf("\n\n");
         encontrado = fread(&usuarios[i], sizeof(ModeloDeLogin), 1, loginFile);
 
         if (encontrado == 0)
@@ -66,8 +158,7 @@ void BuscarUsuario(ModeloDeLogin login)
         }
         else if (strcmp(login.usuario, usuarios[i].usuario) == 0 && strcmp(login.senha, usuarios[i].senha) == 0)
         {
-            printf("\nEncontrei o usuario");
-            break;
+            return true;
         }
         else
         {
@@ -75,13 +166,14 @@ void BuscarUsuario(ModeloDeLogin login)
             continue;
         }
     }
+    return false;
 }
 
-void Login()
+bool Login()
 {
-    struct ModeloDeLogin login;
+    ModeloDeLogin login;
     login = MostrarFormularioLogin(login);
-    BuscarUsuario(login);
+    return BuscarUsuario(login);
 }
 
 void CadastrarUsuario()
@@ -106,10 +198,207 @@ void CadastrarUsuario()
     }
 }
 
+void MostrarMenuCadastroUsuarios()
+{
+    MostrarTitulo("                        Cadastro de usuários                        ");
+    loginFile = AbrirTxt(loginFile, "login.txt", "r");
+
+    int encontrado = 1;
+    int i = 0;
+
+    printf("Usuários:\n");
+    while (encontrado == 1)
+    {
+        encontrado = fread(&usuarios[i], sizeof(ModeloDeLogin), 1, loginFile);
+
+        if (encontrado == 0)
+        {
+            if (i == 0)
+                printf("**Sem usuários cadastrados**");
+
+            break;
+        }
+        else
+        {
+            printf("%d - ", i + 1);
+            printf(usuarios[i].usuario);
+            PularLinha();
+            i++;
+            continue;
+        }
+    }
+
+    LinhaDivisoria();
+    PularLinha();
+    printf("1 - Cadastrar\n");
+    printf("2 - Editar\n");
+    printf("3 - Voltar\n");
+    printf("0 - Sair\n");
+}
+
+void MenuCadastroUsuarios()
+{
+    int resposta;
+    bool tentarNovamente = false;
+
+    do
+    {
+        LimparTela();
+        MostrarMenuCadastroUsuarios();
+        scanf("%d", &resposta);
+        switch (resposta)
+        {
+        case 1:
+            CadastrarUsuario();
+            break;
+        case 2:
+            printf("Editando usuário");
+            break;
+        case 3:
+            printf("Voltando");
+            break;
+        case 0:
+            FecharPrograma();
+            break;
+        default:
+            printf("Opção inválida, tente novamente");
+            break;
+        }
+    } while (tentarNovamente);
+}
+
+// MENU CADASTROS
+void MostrarMenuCadastros()
+{
+    LimparTela();
+    MostrarTitulo("                        Menu de cadastros                      ");
+    printf("1 - Cadastrar atleta\n");
+    printf("2 - Cadastrar equipe medica\n");
+    printf("3 - Cadastrar funcionarios\n");
+    printf("4 - Cadastrar voluntarios\n");
+    printf("5 - Usuários do sistema\n");
+    printf("6 - Voltar\n");
+    printf("0 - Sair\n");
+}
+
+void MenuCadastros()
+{
+    int resposta;
+    bool tentarNovamente = false;
+
+    do
+    {
+        LimparTela();
+        MostrarMenuCadastros();
+        scanf("%d", &resposta);
+        switch (resposta)
+        {
+        case 1:
+            break;
+        case 2:
+            // CadastrarAtleta();
+            break;
+        case 5:
+            MenuCadastroUsuarios();
+            break;
+        case 0:
+            FecharPrograma();
+            break;
+        default:
+            printf("Opção inválida, tente novamente");
+            break;
+        }
+    } while (tentarNovamente);
+}
+
+// MENU PRINCIPAL
+void MostrarMenuPrincipal()
+{
+    printf("O que você deseja fazer?\n\n");
+    printf("1 - Cadastros\n");
+    printf("2 - Relatórios\n");
+    printf("0 - Sair\n");
+}
+
+void MenuPrincipal()
+{
+    int resposta;
+    bool tentarNovamente = false;
+
+    do
+    {
+        LimparTela();
+        MostrarMenuPrincipal();
+        scanf("%d", &resposta);
+        switch (resposta)
+        {
+        case 1:
+            MenuCadastros();
+            break;
+        case 2:
+            // CadastrarEquipeMedica
+            break;
+        case 3:
+            // CadastrarFuncionarios
+        case 4:
+            // CadastrarVoluntarios
+            break;
+        default:
+            printf("Opção inválida, tente novamente");
+            tentarNovamente = true;
+            break;
+        }
+    } while (tentarNovamente);
+}
+
+// MENU INICIAL
+void MostrarMenuInicial()
+{
+    MostrarTitulo("Bem vindo ao gerenciamento das olimpiadas");
+
+    printf("O que você deseja fazer?\n\n");
+    printf("1 - Login\n");
+    printf("2 - Cadastrar\n");
+    printf("0 - Sair\n");
+}
+
+void MenuInicial()
+{
+    int resposta;
+    bool tentarNovamente = false;
+
+    do
+    {
+        LimparTela();
+        MostrarMenuInicial();
+        scanf("%d", &resposta);
+        switch (resposta)
+        {
+        case 1:
+            bool isLogedIn;
+            isLogedIn = Login();
+
+            if (isLogedIn)
+            {
+                MenuPrincipal();
+            }
+            break;
+        case 2:
+            CadastrarUsuario();
+            break;
+        case 0:
+            FecharPrograma();
+            break;
+        default:
+            printf("Opção inválida, tente novamente");
+            break;
+        }
+    } while (tentarNovamente);
+}
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    CadastrarUsuario();
-    Login();
+    MenuInicial();
     return 0;
 }
